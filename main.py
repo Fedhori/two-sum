cf9d4d3eb36d9e71f24cb4e7ee89d0835cd6f461
Fedhori <edhori990611@gmail.com>
2023-10-04 09:35:33 +0000

    implement main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import List
+def twoSum(nums: List[int], target: int) -> List[int]:
+    i = 0
+    length = len(nums)
+    while i < length - 1:
+        j = i + 1
+        while j < length:
+            if(nums[i] + nums[j] == target):
+                return [i, j]
+            j = j + 1
+        i = i + 1
+    return
